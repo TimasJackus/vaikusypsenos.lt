0112cf9edfbe0c1a4104f46203dafc36731298c6
Timas Jackus <timas@mediapark.com>
2020-02-14 00:57:14 +0200

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,37 +8,41 @@ var sticky = header.offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function checkScroll() {
-    console.log(sticky, window.pageYOffset);
-    if (window.pageYOffset > sticky + 25) {
+    if (window.pageYOffset > sticky) {
         header.classList.add("sticky");
     } else {
         header.classList.remove("sticky");
     }
 }
 
-var pswpElement = document.querySelectorAll('.pswp')[0];
-
-// build items array
-var items = [
-    {
-        src: 'https://placekitten.com/600/400',
-        w: 600,
-        h: 400
-    },
-    {
-        src: 'https://placekitten.com/1200/900',
-        w: 1200,
-        h: 900
+function scrollToElement(hash) {
+    const el = document.getElementById(hash);
+    if (el) {
+        const offsetTop = el.offsetTop;
+        window.scrollTo({
+            top: offsetTop - 100,
+            behavior: 'smooth',
+        });
     }
-];
+}
 
-// define options (if needed)
-var options = {
-    // optionName: 'option value'
-    // for example:
-    index: 0 // start at first slide
-};
+if (window.location.href.includes('#')) {
+    const hash = window.location.href.split('#')[1];
+    scrollToElement(hash);
+}
 
-// Initializes and opens PhotoSwipe
-var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
-gallery.init();
+function toggleMenu(event) {
+    const nav = document.getElementById('nav');
+    console.log(nav.classList.contains('open'));
+    if (nav.classList.contains('open')) {
+        nav.classList.remove('open');
+    } else {
+        nav.classList.add('open');
+    }
+}
+
+function locationHashChanged() {
+    toggleMenu();
+}
+
+window.onhashchange = locationHashChanged;
